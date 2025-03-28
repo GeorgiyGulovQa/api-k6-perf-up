66e11f65ab0a50432a3a27367f41980bfcca4da7
Georgiy Gulov <georgiy.gulov@zfort.com>
2025-03-28 17:58:19 +0200

    Added template for random UUID creation function

---
+++ utils/Random_UUID_Function.js
@@ -0,0 +1,10 @@
+function uuidV4() {
+    const uuid = new Array(36);
+    for (let i = 0; i < 36; i++) {
+      uuid[i] = Math.floor(Math.random() * 16);
+    }
+    uuid[14] = 4;
+    uuid[19] = (uuid[19] & 0x3) | 0x8;
+    uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+    return uuid.map((x) => x.toString(16)).join('');
+  };
